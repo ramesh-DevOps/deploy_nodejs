1cecf2b2a009f5caab85e3cd1f4a7fa83fc5fd7a
ramesh-DevOps <73949769+ramesh-DevOps@users.noreply.github.com>
2024-09-10 16:32:27 +0530

    Update helm-deploy.sh

--- scripts/helm-deploy.sh
+++ scripts/helm-deploy.sh
@@ -6,12 +6,23 @@ HELM_CHART="my-node-app-chart"
 IMAGE="my-node-app"
 NAMESPACE="default"
 
+
+# Check if all required arguments are provided
+if [ -z "$ACR_NAME" ] || [ -z "$HELM_CHART" ] || [ -z "$IMAGE" ] || [ -z "$NAMESPACE" ]; then
+  echo "Usage: $0 <acr-name> <helm-chart-path> <image-name:tag> <namespace>"
+  exit 1
+fi
+
 # Login to ACR
+echo "Logging in to ACR: $ACR_NAME"
 az acr login --name $ACR_NAME
 
 # Add Helm repository
-helm repo add my-acr https://$ACR_NAME.azurecr.io/helm/v1/repo
+echo "Adding Helm repository for ACR"
+helm repo add my-acr oci://$ACR_NAME.azurecr.io/helm
 helm repo update
 
 # Install or upgrade Helm chart
+echo "Deploying Helm chart: $HELM_CHART"
 helm upgrade --install my-release $HELM_CHART --namespace $NAMESPACE --set image.repository=$IMAGE
+
